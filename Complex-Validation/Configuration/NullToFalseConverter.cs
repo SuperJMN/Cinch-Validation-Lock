ab50ebbbbb2ab81c9e37bb2825c2ab8405b0490a
José Manuel Nieto Sánchez <superjmn@outlook.com>
2015-11-25 08:33:52 +0100

    Moving

---
+++ Complex-Validation/Configuration/NullToFalseConverter.cs
@@ -0,0 +1,19 @@
+namespace TestListBoxCachonda
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class NullToFalseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
